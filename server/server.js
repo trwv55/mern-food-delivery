1973968281bf607199739a61f099d02413376c0c
Vitalii Travin <vitalii.trvn@gmail.com>
2023-01-10 18:57:59 +0600

    auth backend

--- server/server.js
+++ server/server.js
@@ -3,12 +3,17 @@ import data from './data.js';
 import jwt from 'jsonwebtoken';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+import { validationResult } from 'express-validator';
+import { registerValidation } from './validations/reg.js';
+import UserModel from './models/user.js';
 
 const app = express();
 
 mongoose
   .connect(
-    'mongodb+srv://danfood:danfood@cluster0.6xdsrm0.mongodb.net/?retryWrites=true&w=majority',
+    'mongodb+srv://danfood:danfood@cluster0.6xdsrm0.mongodb.net/food?retryWrites=true&w=majority',
   )
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
@@ -22,6 +27,50 @@ app.get('/api/products', (req, res) => {
 
 app.get('/', (req, res) => {
   res.send('hi!');
+});
+
+app.post('/register', registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      name: req.body.name,
+      email: req.body.email,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret',
+      {
+        expiresIn: '30d',
+      },
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...user._doc,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Не удалось зарегестрироваться',
+    });
+  }
 });
 
 app.post('/login', (req, res) => {
---
+++ server/validations/reg.js
@@ -0,0 +1,12 @@
+import { body } from 'express-validator';
+
+export const registerValidation = [
+  body('email', 'Неверный формат почты').isEmail(),
+  body(
+    'password',
+    'Пароль должен быть минимум 6 символов, содержать одну заглавную и строчную буквы',
+  )
+    .isLength({ min: 6 })
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/),
+  body('name', 'Укажите имя').isLength({ min: 3 }),
+];
